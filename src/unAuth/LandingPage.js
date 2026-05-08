03084c9ef6a8f849fba172ab0b3395562d0b71c4
Nick <nicolas.lindsay06@gmail.com>
2026-05-08 17:01:48 -0500

    Landing Page Render

---
+++ src/unAuth/LandingPage.js
@@ -0,0 +1,12 @@
+import "./LandingPage.css";
+
+function LandingPage() {
+  return (
+    <main className="landing-page">
+      <h1 className="landing-title">Hello Page</h1>
+      <p className="landing-subtitle">Welcome to your React landing page.</p>
+    </main>
+  );
+}
+
+export default LandingPage;
